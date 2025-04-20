2d680231e8eff4c2f1596ebf32d33463fd95aa43
Srimannarayana <87487895+Srimannarayana2021@users.noreply.github.com>
2025-04-20 10:33:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const makeChange = (c) => {
   // your name here
+	c = Number(c); 
+
+  const change = {};
+  change.q = Math.floor(c / 25);
+  c %= 25;
+
+  change.d = Math.floor(c / 10);
+  c %= 10;
+
+  change.n = Math.floor(c / 5);
+  c %= 5;
+
+  change.p = c;
+
+  return change;
 };
 
 // Do not the change the code below
